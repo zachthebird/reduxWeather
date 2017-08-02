44c5b333df7fb41b4a1d4933053ff8caaa2c06cd
Zach Bird <zachthebird@gmail.com>
2017-08-02 12:13:39 -0500

    searchBar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/SearchBar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
